5edf0f1aaf1b381c51ce5fa79560c9347f4b1975
Louis <louis1204@gmail.com>
2014-02-08 13:38:49 -0800

    Rest

--- pa2review.ml
+++ pa2review.ml
@@ -1,11 +1,31 @@
-(* assoc *)
+(* assoc - finds the first matching key and returns value o.w. value *)
 let rec assoc(value, key, list)= match list with
 |[] -> value
 | (key', value')::tail -> if key = key' then value'
                   else assoc(value, key, tail);;  
 
 (* remove duplicates *)
-let rec removeDuplicates(list) = match List.rev(list) with
-|[]->[]
-|head::tail -> if List.mem head tail then removeDuplicates(List.rev(tail))
-               else List.rev(head::removeDuplicates(List.rev(tail)));;
+let removeDuplicates(list') =
+  let rec removeDuplicatesHelper(list) = match List.rev(list) with
+  |[]->[]
+  |head::tail -> if List.mem head tail then 
+                  removeDuplicatesHelper(List.rev(tail))
+                 else [head]@removeDuplicatesHelper(List.rev(tail))
+  in List.rev(removeDuplicatesHelper(list'));;
+
+(* wwhile - f returns (result, boolean) while boolean wwhile(f, result) 
+o.w. result.
+
+Pretty much we just keep repeating f on its own result until we get false,
+which will then get our result*)
+
+let f x = let xx = x*x*x in (xx, xx < 100);;
+
+let rec wwhile(f, x) = match f(x) with
+|(result, boolean) -> if boolean then wwhile(f, result)
+                      else result;;
+
+(* fixpoint *)
+let rec fixpoint(f, x) =
+if f(x) = x && f(f(x)) = f(x) then f(x)
+else fixpoint(f, f(x));;
